23e65d22d9709a7a955aeff60fc3ceb3cd2fe106
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2021-04-07 10:39:20 +0900

    fix: package exporter to latest version

--- src/RandomFixtureKit.Unity/Assets/Scripts/Editor/PackageExporter.cs
+++ src/RandomFixtureKit.Unity/Assets/Scripts/Editor/PackageExporter.cs
@@ -11,16 +11,15 @@ public static class PackageExporter
     [MenuItem("Tools/Export Unitypackage")]
     public static void Export()
     {
-        var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
-
-        // configure
         var root = "Scripts/RandomFixtureKit";
-        var fileName = string.IsNullOrEmpty(version) ? "RandomFixtureKit.Unity.unitypackage" : $"RandomFixtureKit.Unity.{version}.unitypackage";
+        var version = GetVersion(root);
+
+        var fileName = string.IsNullOrEmpty(version) ? "RandomFixtureKit.unitypackage" : $"RandomFixtureKit.{version}.unitypackage";
         var exportPath = "./" + fileName;
 
         var path = Path.Combine(Application.dataPath, root);
         var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
-            .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".asmdef" || Path.GetExtension(x) == ".json")
+            .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".asmdef" || Path.GetExtension(x) == ".json" || Path.GetExtension(x) == ".meta")
             .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
             .ToArray();
 
@@ -32,6 +31,42 @@ public static class PackageExporter
             ExportPackageOptions.Default);
 
         UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+    }
+
+    static string GetVersion(string root)
+    {
+        var version = Environment.GetEnvironmentVariable("UNITY_PACKAGE_VERSION");
+        var versionJson = Path.Combine(Application.dataPath, root, "package.json");
+
+        if (File.Exists(versionJson))
+        {
+            var v = JsonUtility.FromJson<Version>(File.ReadAllText(versionJson));
+
+            if (!string.IsNullOrEmpty(version))
+            {
+                if (v.version != version)
+                {
+                    var msg = $"package.json and env version are mismatched. UNITY_PACKAGE_VERSION:{version}, package.json:{v.version}";
+
+                    if (Application.isBatchMode)
+                    {
+                        Console.WriteLine(msg);
+                        Application.Quit(1);
+                    }
+
+                    throw new Exception("package.json and env version are mismatched.");
+                }
+            }
+
+            version = v.version;
+        }
+
+        return version;
+    }
+
+    public class Version
+    {
+        public string version;
     }
 }
 
